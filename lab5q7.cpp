29b499124ab41ba3e48c744e2df34e3e591293e7
isha-dwivedi <42205291+isha-dwivedi@users.noreply.github.com>
2018-08-31 01:46:06 -0400

    Add files via upload

---
+++ lab5q7.cpp
@@ -0,0 +1,20 @@
+//print multiplication table
+//library
+#include<iostream>
+using namespace std;
+int main()
+{
+//declare vthe variables
+int a;
+//ask the user to enter the no.
+cout<<"enter the no. whose table is to be printed";
+cin>>a;
+//put the for loop
+for(int i=1; i<=10; i++)
+//display the table
+{cout<<a<<"*"<<i<<"="<<a*i<<endl;
+}
+}
+
+
+
